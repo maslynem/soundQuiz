3b19957a0f3a8139020064e1a3c8f70dc7be9b16
Vitaly Saparin <129934921+maslynem@users.noreply.github.com>
2023-11-10 09:42:03 +0300

    Add files via upload

---
+++ src/test/java/ru/maslynem/quiz/sound/SoundApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maslynem.quiz.sound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
