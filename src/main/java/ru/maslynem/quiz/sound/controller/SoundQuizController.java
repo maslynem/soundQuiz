e1df76a85c01dd99854e66a9efaee44d6c61c288
Saparin Vitaly <vit-1998@inbox.ru>
2023-11-24 14:56:34 +0300

    add logger to controller

--- src/main/java/ru/maslynem/quiz/sound/controller/SoundQuizController.java
+++ src/main/java/ru/maslynem/quiz/sound/controller/SoundQuizController.java
@@ -1,6 +1,7 @@
 package ru.maslynem.quiz.sound.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.maslynem.quiz.sound.controller.request.QuizRequest;
 import ru.maslynem.quiz.sound.controller.response.TopicResponse;
@@ -13,6 +14,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class SoundQuizController {
     private final TopicService topicService;
     private final QuizGeneratorService quizGeneratorService;
@@ -20,11 +22,13 @@ public class SoundQuizController {
     @GetMapping
     public TopicResponse getAllTopics() {
         List<String> topics = topicService.findAllTopics();
+        log.info("Get all topics request. Topics: {}", topics);
         return new TopicResponse(topics.size(), topics);
     }
 
     @PostMapping
     public Map<String, List<String>> getQuizByTopics(@RequestBody QuizRequest request) {
+        log.info("Get quiz by topics. Topics: {}", request.getTopics());
         return quizGeneratorService.generateQuiz(request);
     }
 
